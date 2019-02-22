176517028ef51a269ce23ba1af4e01df95b969c8
DukeChiang <dukechiang@qq.com>
2019-02-22 13:59:29 +0800

    新增FUI资源url代码生成功能

--- Unity/Assets/Hotfix/Module/FairyGUI/AutoGeneratedCode/Common/PackageCommon.cs
+++ Unity/Assets/Hotfix/Module/FairyGUI/AutoGeneratedCode/Common/PackageCommon.cs
@@ -4,6 +4,11 @@ namespace ETHotfix
 {
 	public static partial class FGUIPackage
 	{
-		public static string Common = "Common";
+		public const string Common = "Common";
+		public const string Common_LoadingProgressBar = "ui://Common/LoadingProgressBar";
+		public const string Common_ui_common_bg_loading = "ui://Common/ui_common_bg_loading";
+		public const string Common_ui_common_jindutiao_bar = "ui://Common/ui_common_jindutiao_bar";
+		public const string Common_ui_common_jindutiao_bg = "ui://Common/ui_common_jindutiao_bg";
+		public const string Common_Loading = "ui://Common/Loading";
 	}
 }
